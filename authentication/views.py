0de61b6953b9af8a8b34311704c72ae8efee9709
Joy Kirii <joykirii@gmail.com>
2021-06-08 20:59:58 +0300

    create update user view function

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import User
+from rest_framework.generics import RegistrationAPIView
 
-from .serializers import RegistrationSerializer,LoginSerializer
+from .serializers import RegistrationSerializer,LoginSerializer,UserSerializer
 # from .renderers import UserJSONRenderer
 
 # Create your views here.
@@ -47,4 +48,24 @@ class LoginAPIView(APIView):
     serializer=self.serializer_class(User.objects.all(),many=True)
     return Response(serializer.data,status=status.HTTP_200_OK)
 
-  
+class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+  permission_classes = (IsAuthenticated,)
+  renderer_classes = (UserJSONRenderer,)
+  serializer_class = UserSerializer
+
+  def retrieve(self, request, *args, **kwargs): 
+    serializer = self.serializer_class(request.user)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+  def update(self, request, *args, **kwargs):
+    serializer_data = request.data.get('user', {})
+
+    serializer = self.serializer_class(
+      request.user, data=serializer_data, partial=True
+    )
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+    
