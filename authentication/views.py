36bb6183671c4bb35684a9de09489db0a4467913
Joy Kirii <joykirii@gmail.com>
2021-06-08 18:55:43 +0300

    render user objects

--- authentication/views.py
+++ authentication/views.py
@@ -3,20 +3,25 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import User
 
 from .serializers import RegistrationSerializer
 
 # Create your views here.
 class RegistrationAPIView(APIView):
-  permission_classes = (AllowAny,)
+  permission_classes = [AllowAny,]
   serializer_class = RegistrationSerializer
 
-  def post(self, request):
-    user = request.data.get('user', {})
-
+  def post(self, request,format=None):
+    # user = request.data.get(User, {})
+    user = request.data
+    print(user)
     serializer = self.serializer_class(data=user)
     serializer.is_valid(raise_exception=True)
     serializer.save()
 
     return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+
+  def get(self,request, *args, **kwargs):
+    serializer=self.serializer_class(User.objects.all(),many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
