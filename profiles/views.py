6db964df9c779a5314086c3f7833b20b45b253c6
Joy Kirii <joykirii@gmail.com>
2021-06-08 22:14:01 +0300

    write create profile function

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import status
 
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .models import Profile
+
+from .serializers import ProfileSerializer
+from django.http import Http404
 # Create your views here.
+class ProfileRetrieveAPIView(RetrieveAPIView):
+  permission_classes = (AllowAny,)
+  renderer_classes = (ProfileJSONRenderer,)
+  serializer_class = ProfileSerializer
+
+  def retrieve(self, request, username, *args, **kwargs):
+    try:
+      profile = Profile.objects.select_related('user').get(
+        user__username=username
+      )
+    except Profile.DoesNotExist:
+      raise HTTP404()
+
+    serializer = self.serializer_class(profile)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
