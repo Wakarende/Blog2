b7de322888d768c638017395038b6a9502013678
Joy Kirii <joykirii@gmail.com>
2021-06-09 21:43:24 +0300

    update user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import User
 # from django.contrib.auth.models import User
 from blogs.serializers import ArticleSerializer
 
@@ -10,53 +10,71 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
       
-class RegistrationSerializer(serializers.ModelSerializer):
-  username=serializers.CharField()
-  email=serializers.EmailField()
-  password=serializers.CharField(
-    min_length=8,
-    max_length=20,
-    write_only=True,
-    error_messages={
-      "min_length": "Password should be atleast {min_length} characters"
-    }
+# class RegistrationSerializer(serializers.ModelSerializer):
+#   username=serializers.CharField()
+#   email=serializers.EmailField()
+#   password=serializers.CharField(
+#     min_length=8,
+#     max_length=20,
+#     write_only=True,
+#     error_messages={
+#       "min_length": "Password should be atleast {min_length} characters"
+#     }
 
-  )
-  confirmpassword=serializers.CharField(
-    min_length=8,
-    max_length=20,
-    write_only=True,
-    error_messages={
-      "min_length": "Password should be atleast {min_length} characters"
-    }
-  )
+#   )
+#   confirmpassword=serializers.CharField(
+#     min_length=8,
+#     max_length=20,
+#     write_only=True,
+#     error_messages={
+#       "min_length": "Password should be atleast {min_length} characters"
+#     }
+#   )
 
+#   class Meta:
+#     model=User
+#     fields=["username", "email", "password", "confirmpassword"]
+
+#   def create(self, validated_data):
+#     user=User.objects.create(
+#       username=validated_data['username'],
+#       email=validated_data['email']
+#     )
+#     user.set_password(validated_data['password'])
+#     user.save()
+#     return user
+
+# class LoginSerializer(serializers.ModelSerializer):
+#   username=serializers.CharField()
+#   password=serializers.CharField(
+#     min_length=8,
+#     max_length=20,
+#     write_only=True,
+#     error_messages={
+#       "min_length": "Password should be atleast {min_length} characters"
+#     }
+
+#   )
+#   class Meta:
+#     model=User
+#     fields=["username", "password"]
+
+class UserSerializer(serializers.ModelSerializer):
   class Meta:
-    model=User
-    fields=["username", "email", "password", "confirmpassword"]
+    model = User
+    fields = ['id', 'username','email', 'password']
+    extra_kwargs = {
+      'password': {'write_only': True, }
+    }
 
   def create(self, validated_data):
-    user=User.objects.create(
-      username=validated_data['username'],
-      email=validated_data['email']
-    )
-    user.set_password(validated_data['password'])
-    user.save()
-    return user
+    password = validated_data.pop('password', None)
+    instance = self.Meta.model(**validated_data)
+    if password is not None:
+      instance.set_password(password)
+    instance.save()
+    return instance
 
-class LoginSerializer(serializers.ModelSerializer):
-  username=serializers.CharField()
-  password=serializers.CharField(
-    min_length=8,
-    max_length=20,
-    write_only=True,
-    error_messages={
-      "min_length": "Password should be atleast {min_length} characters"
-    }
-
-  )
-  class Meta:
-    model=User
-    fields=["username", "password"]
+    
 
 
